967bacbd123ac5630bed136749f6dc9d12933515
thiagoc-machado <thiagocmachado@yahoo.com.br>
2022-11-10 12:28:02 +0100

    workflow

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,17 +11,20 @@ urlpatterns = [
 
     # path for about view
     #path(route='/about', view=views.get_dealerships, name='about'),
-    path(route='about', view=views.contact, name='about'),
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
     #path(route='/contact', view=views.get_dealerships, name='contact'),
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
-
+     path('registration/', views.registration_request, name='registration'),
+  
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
